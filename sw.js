3236adb21065c0bffeff343e5025a721b7927306
jorgeomomo-dotcom <jorgeomomo@gmail.com>
2025-12-22 00:16:27 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,34 @@
-const cacheName = 'v1-electrical-calc';
-const assets = ['./index.html'];
+const CACHE_NAME = "sonelgaz-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
 
-self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open(cacheName).then(cache => {
-            return cache.addAll(assets);
-        })
-    );
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(res => {
-            return res || fetch(e.request);
-        })
-    );
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((res) => {
+      return res || fetch(evt.request);
+    })
+  );
 });
